fa2a3d618766651849faad06809e872a5a89618b
ShuvamKhadka <khadkashuvam4@gmail.com>
2026-06-30 07:12:55 +0545

    day4 session

---
+++ day4.py
@@ -0,0 +1,36 @@
+#from day4test import add_number
+from day4test import calc
+# def function_name(name, age):
+#     print(f"name is: {name}")
+#     print(f"age is: {age}")
+
+# name = "ram"
+# age = 20
+# function_name(name, age)
+# function_name(age=age, name=name)
+
+# def function_name(x, y, z=20):
+#     print(f"the value of x is: {x} y is:{y} z is:{z}")
+#     print(x+y+z)
+
+# x = 40
+# y = 50
+# z = 30
+# function_name(x, y)
+# function_name(x, y, z)
+
+# from day4test import add_number
+
+# result = add_number(20, 30)
+# print(result)
+
+# first_num = int(input("Enter your number: "))
+# second_num = int(input("Enter your number: "))
+# result = first_num + second_num
+# print(result)
+
+first_num = int(input("Enter your number: "))
+# second_num = int(input("Enter your number: "))
+# op = input("Enter your operator: ")
+# result = calc(first_num, second_num, op)
+# print(result)
