93b113b9c66bbe3405d8e0b392761e23cee06cd3
ShuvamKhadka <khadkashuvam4@gmail.com>
2026-07-01 20:10:43 +0545

    5th session

--- day5.py
+++ day5.py
@@ -1,37 +1,37 @@
 from day5test import calculator
 #args and kwargs
 
-# def mul_no(*args, **kargs):
-#     print(args)
-#     print(kargs)
+def mul_no(*args, **kargs):
+    print(args)
+    print(kargs)
    
-# mul_no(2, 4, 6, value = 50)
+mul_no(2, 4, 6, value = 50)
 
-# def add_no(*args, **kargs):
-#     sum = 0
-#     for i in args:
-#         sum += i
-#     print("The sum of the numbers is:", sum)
+def add_no(*args, **kargs):
+    sum = 0
+    for i in args:
+        sum += i
+    print("The sum of the numbers is:", sum)
 
-# add_no(10, 20, 30, 40)
+add_no(10, 20, 30, 40)
     
-# def mul_no(*args, **kargs):
-#     print(args)
-#     print(kargs)
+def mul_no(*args, **kargs):
+    print(args)
+    print(kargs)
    
-# mul_no(20, 40, 60, 50, value = 50, name = "Prabin")
+mul_no(20, 40, 60, 50, value = 50, name = "Prabin")
 
-# def mul_no(a, *args, **kargs):
-#     print(args)
-#     print(kargs)
+def mul_no(a, *args, **kargs):
+    print(args)
+    print(kargs)
    
-# mul_no(20, 40, 60, 50, value = 50, name = "Prabin")
+mul_no(20, 40, 60, 50, value = 50, name = "Prabin")
 
-# def mul_no(a, b,*args, **kargs):
-#     print(args)
-#     print(kargs)
+def mul_no(a, b,*args, **kargs):
+    print(args)
+    print(kargs)
    
-# mul_no(20, 40, 60, 50, value = 50, name = "Prabin")
+mul_no(20, 40, 60, 50, value = 50, name = "Prabin")
 
 #numnber1, number2, operator 
 number1 = int(input("Enter the first number: "))
