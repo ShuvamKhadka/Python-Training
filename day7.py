5826ff5a72130239d8355a3e30d6b1e0418d53d7
ShuvamKhadka <khadkashuvam4@gmail.com>
2026-07-28 21:03:51 +0545

    push

---
+++ day7.py
@@ -0,0 +1,29 @@
+# class Student:
+
+#     def user_detail(self):
+#         print("this is user details.")
+
+#     def total_mark(self):
+#         print("this is total mark.")
+
+#     def show_course(self):
+#         print("this shows total course.")
+
+# s1 = Student() 
+# s1.user_detail()
+
+# class Calculator:
+
+#     def __init__(self, name, address):
+#         self.name = name
+#         self.address = address
+
+#     def std_details(self):
+#         print(f"Student : {self.name} , address : {self.address}")
+
+#     def grade(self):
+#         print("good grades.")
+
+# t1 = Calculator("Aayush", "Kathmandu")
+# t1.std_details()
+
